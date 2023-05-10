ee06ba1f3922bc0d447ba251da4ca84427228c54
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2023-05-10 20:39:26 +0300

    add sessionValidator function

--- src/session/session.service.ts
+++ src/session/session.service.ts
@@ -37,7 +37,7 @@ export class SessionService {
             return
         }
 
-        const responseDTO = await this.rabbitResponseLogic(JSON.stringify(new RequestServiceDTO(data)))
+        const responseDTO = await this.rabbitResponseLogic(new RequestServiceDTO(data))
 
         const deltaTime = Date.now() - startDate
         console.log("Запрос выполнен за " + deltaTime + " ms. status: " + responseDTO.status)//cтатус
@@ -50,7 +50,7 @@ export class SessionService {
         return ''
     }
 
-    async rabbitResponseLogic(data: string): Promise<ResponseDTO> {
+    async rabbitResponseLogic(data: RequestServiceDTO): Promise<ResponseDTO> {
         const responseDTO = new ResponseDTO();
 
         try {
@@ -92,6 +92,47 @@ export class SessionService {
         else {
             return false
         }
+    }
+
+    //------------Перенести в другой сервис!!!------------>
+
+    async isSessionValid(sessionId: number, sessionHash: string) : Promise<boolean> {
+        const requestServiceDTO = new RequestServiceDTO(JSON.stringify({ userId: '', sessionHash: sessionHash, sessionId: sessionId}))
+
+        if ((await this.requestValidSession(requestServiceDTO)).status == 200){
+            true
+        }
+        return false
+    }
+
+    async requestValidSession(data: RequestServiceDTO) : Promise<ResponseDTO>{ //вынести 
+        const responseDTO = new ResponseDTO();
+
+        try {
+            const response = await this.client.send("validate_session", data).pipe(timeout(5000)).toPromise()
+
+            const json = JSON.parse(JSON.stringify(response))
+
+            const responseServiceDTO = new ResponseServiceDTO(json.status, json.data)
+
+            responseDTO.data = responseServiceDTO.data
+            responseDTO.status = responseServiceDTO.status
+        } catch (e) {
+            if (e.message == 'Timeout has occurred') {
+                responseDTO.status = 408
+                console.log("TIMEOUT")
+            }
+            else if (e.err.code == 'ECONNREFUSED') {
+                responseDTO.status = 408
+                this.client.close()
+                console.log("ECONNREFUSED")
+            } else {
+                responseDTO.status = 400
+                console.log("Ошибка не обрабатывается")
+                console.log(e)
+            }
+        }
+        return responseDTO
     }
 
 }
