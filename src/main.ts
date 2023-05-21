0157581af2ffe53bf60d1f51cdace1f6660ee272
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2023-05-21 17:37:01 +0300

    cors oprions

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,20 @@ import { AppModule } from './app.module';
 import * as fs from 'fs';
 
 async function bootstrap() {
+    const corsOptions = {
+        "origin": "*",
+        "methods": "GET,POST",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+    }
+
     const httpsOptions = {
         key: fs.readFileSync(__dirname + '/secrets/privkey.pem'),
         cert: fs.readFileSync(__dirname + '/secrets/fetchain.pem')
     }
 
     const app = await NestFactory.create(AppModule, { httpsOptions });
-    app.enableCors()
+    app.enableCors(corsOptions)
 
     await app.listen(9600);
 }
