e96872ea0d74a532659291637fa4324bdc698b56
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2023-05-24 17:59:24 +0300

    xml recponse

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -61,35 +61,38 @@ export class PaymentsService {
 
     async okKallbackGetResponser(params: any, res: Response) {
         const startDate = Date.now()
-        const responseDTO = new ResponseDTO()
         let status = 200
         let msg = 'OK'
 
+        let responce = '<?xml version="1.0" encoding="UTF-8"?><callbacks_payment_response xmlns="http://api.forticom.com/1.0/">true</callbacks_payment_response>'
+
         try {
-            const responseServiceDTO = await this.okKallbackGetHandler(params)
-            responseDTO.data = responseServiceDTO.data
+            status = await this.okKallbackGetHandler(params)
         } catch (e) {//прописать разные статусы
             if (e == 403 || e == 'parsing error') {
                 status = 403//перезагрузить клиент
                 msg = e
             } else if (e == 'timeout' || e == 'ECONNREFUSED') {
-                status = 408//повторить запрос
+                status = 2//повторить запрос
                 msg = e
             } else {
                 status == 400//хз че делать
                 msg = 'Неизвестная ошибка. Статус: ' + status
             }
             console.log("--->Ошибка " + e)
+            responce = '<?xml version="1.0" encoding="UTF-8"?><ns2:error_response xmlns:ns2="http://api.forticom.com/1.0/"><error_code>2</error_code><error_msg>CALLBACK_INVALID_PAYMENT : Payment is invalid and can not be processed</error_msg></ns2:error_response>'
         }
 
-        res.status(status).json(responseDTO)
+        res.header("Content-Type", "application/xml");
+
+        res.status(status).json(responce)
 
         const deltaTime = Date.now() - startDate
         this.monitoringService.sendLog('gateway-okCallback', 'get', status, msg, JSON.stringify(params), deltaTime)
         return
     }
 
-    private async okKallbackGetHandler(params: any): Promise<ResponseServiceDTO> {
+    private async okKallbackGetHandler(params: any): Promise<number> {
         let data = {};
         try {
             const json = JSON.parse(JSON.stringify(params))
@@ -100,12 +103,12 @@ export class PaymentsService {
         return this.okKallbackGetLogic(data)
     }
 
-    private async okKallbackGetLogic(data: object) {
+    private async okKallbackGetLogic(data: object) : Promise<number> {
         const responseServiceDTO = await this.rabbitService.questionerPayments(new RequestServiceDTO(data), TypesQueue.OK_CALLBACK)
         if (responseServiceDTO.status != 200) {
             console.log('okCalback servise send status: ' + responseServiceDTO.status)
             throw 403
         }
-        return responseServiceDTO
+        return 200
     }
 }
