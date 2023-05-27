3833d775150bdc3b8c2353303fe82ac6243e39af
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2023-05-27 17:41:13 +0300

    test

--- src/others/midlevares/data-integrity/data-integrity.middleware.ts
+++ src/others/midlevares/data-integrity/data-integrity.middleware.ts
@@ -12,54 +12,34 @@ export class DataIntegrityMiddleware implements NestMiddleware {
     private readonly monitoringService: MonitoringService
 
     use(req: Request, res: Response, next: NextFunction) {
-        let hash = ''
         let data = {}
-        console.log(typeof req.headers)
-        console.log(req.headers)
-
-        let header
-
+        let dataHash = ''
         try {
-            header = JSON.parse(JSON.stringify(req.headers))
-        } catch { console.log('parse headerError') }
-
-        console.log(header.sessionid)
-        console.log(header.sessionhash)
-        console.log(header.datahash)
-
-        console.log(req.headers['sessionid'])
-        console.log(req.headers['sessionhash'])
-        console.log(req.headers['datahash'])
-        console.log('---------------------')
-
-        if (req.method == "POST") {
-            try {
-                hash = req.body.hash
-                data = req.body.data
-            } catch (e) {
-                this.monitoringService.sendLog('gateway-data-integrity-midlevare', 'post', 403, 'parsing error', req.body)
-                res.status(403).send("parsing error1")
-                return
+            if (typeof req.headers.datahash == 'string') {
+                dataHash = req.headers.datahash
             }
-        }
-        else if (req.method == "GET") {
-            if (typeof req.query.dto == 'string') {
-                const json = JSON.parse(req.query.dto)
-                hash = json.hash;
-                data = json.data
-            } else {
-                this.monitoringService.sendLog('gateway-data-integrity-midlevare', 'get', 403, 'parsing error', req.body)
-                res.status(403).send("parsing error get 1")
-                return
+
+            if (req.method == "POST") {
+                data = req.body
             }
+            else if (req.method == "GET") {
+                if (typeof req.query.dto == 'string') {
+                    const json = JSON.parse(req.query.dto)
+                    data = json.data
+                }
+            }
+        } catch {
+            this.monitoringService.sendLog('gateway-data-integrity-midlevare', 'parsing', 403, 'bad', req.body)
+            res.status(403).send()
+            return
         }
 
-        if (this.cryptoService.isHashBad(hash, data)) {
-            this.monitoringService.sendLog('gateway-data-integrity-midlevare', 'hash-validator', 403, 'hash bad', req.body)
-            console.log("hash bad1")
-            console.log(hash)
+        if (this.cryptoService.isHashBad(dataHash, data)) {
+            this.monitoringService.sendLog('gateway-data-integrity-midlevare', 'validator', 403, 'bad', req.body)
+            console.log("hash bad")
+            console.log(dataHash)
             console.log(data)
-            res.status(403).send("hash bad")
+            res.status(403).send()
             return
         }
         next();
