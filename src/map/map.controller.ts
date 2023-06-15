37a6732ab738128656f7aeee4e6d22e9b860431b
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2023-06-15 11:55:11 +0300

    f

--- src/map/map.controller.ts
+++ src/map/map.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Res, Get, Query } from '@nestjs/common';
+import { Controller, Res, Get, Query, Headers } from '@nestjs/common';
 import { MapService } from './map.service';
 import { Response } from 'express';
 
@@ -8,9 +8,9 @@ export class MapController {
     constructor(private readonly mapService: MapService) { }
 
     @Get()
-    dataGetStorage(@Query('dto') params: string, @Res() res: Response) {
-        console.log(res.header)
-        console.log(JSON.stringify(res.header))
+    dataGetStorage(@Query('dto') params: string, @Res() res: Response, @Headers() headers: any) {
+        console.log(headers)
+        console.log(JSON.stringify(headers))
         return this.mapService.getMapResponser(params, res)
     }
 }
